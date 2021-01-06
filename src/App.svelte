befce7cf44b5dcd9f35a81bbeb26dff34202046a
R-Technology <43551882+Farizal97@users.noreply.github.com>
2021-01-07 04:37:50 +0700

    namabebas

---
+++ src/App.svelte
@@ -0,0 +1,65 @@
+<script>
+	import {fade,fly} from 'svelte/transition';
+	import EmojiDisplay from './EmojiDisplay.svelte';
+	import Emojiescription from './EmojiDescription.svelte';
+import EmojiDescription from './EmojiDescription.svelte';
+	import Button from './Button.svelte';
+
+	let isLoaded = false;
+	let currentEmoji = '😊';
+	const emojis =  ['😉','🚀'];
+	let m = {x:0,y:0};
+
+	function randomizeEmoji(){
+		return emojis[Math.floor(Math.random() * emojis.length)];
+	}
+
+	function handleRandomButton() {
+		currentEmoji = randomizeEmoji();
+	}
+
+	setTimeout(function()  {
+		isLoaded = true;
+	}, 2500);
+
+	function handleMousemove(event){
+		m.x = event.clientX;
+		m.y = event.clientY;
+	}
+
+</script>
+
+
+
+<style>
+	div {
+		margin:2em;
+	}
+</style>
+
+<svelte:head>
+	<link  href='/terminal.min.css' rel='stylesheet' />
+</svelte:head>
+
+<div class="container" on:mousemove={handleMousemove}>
+	<p>
+		The mouse position: {m.x} x {m.y}
+	</p>
+<h1>Random Emoji</h1>
+<ul>
+	{#each emojis as emoji}
+	<li>{emoji}</li>
+	{/each}
+</ul>
+{#if isLoaded === true}
+<div in:fly={{ y:200,duration:2000 }} out:fade>
+<EmojiDisplay  {currentEmoji} />
+<EmojiDescription />
+<Button on:click={handleRandomButton} title={'Randomize'} />
+</div>
+{:else}
+<h2>Loading...</h2>
+{/if}
+
+<Button title={'Toggle'} on:click={() => isLoaded = !isLoaded} />
+</div>
---
+++ src/EmojiDisplay.svelte
@@ -0,0 +1,30 @@
+<script>
+    export let  currentEmoji = emoji;
+    let current = 'button1';
+	// setTimeout(function() { currentEmoji = '\u{1F625}'; 
+	// }, 1000);
+</script>
+
+<style>
+    div {
+        font-size: 3em;
+        margin-bottom: 0.5em;
+    }
+</style>
+
+<div>{currentEmoji}</div>
+<button
+class="btn" 
+class:btn-primary={current === 'button1'} 
+on:click={() => current = 'button1'}>button 1
+</button>
+<button 
+class="btn" 
+class:btn-primary={current === 'button2'} 
+on:click={() => current = 'button2'}>button 2
+</button>
+<button 
+class="btn" 
+class:btn-primary={current === 'button3'} 
+on:click={() => current = 'button3'}>button 3
+</button>
